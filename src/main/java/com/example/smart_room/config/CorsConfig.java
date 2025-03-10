63ef21958a842a52154edd18c343e02a2562d5da
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2025-03-10 21:47:51 +0700

    fix/ssss

---
+++ src/main/java/com/example/smart_room/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.smart_room.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(false); // Disable credentials
+    }
+}
