a1292c7efcacbb5a74f9d7c85ff56c95c960940e
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2025-04-10 13:58:41 +0700

    update compose

--- src/main/java/com/example/smart_room/config/FirebaseConfig.java
+++ src/main/java/com/example/smart_room/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.FirebaseDatabase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.io.FileInputStream;
@@ -15,9 +16,10 @@ public class FirebaseConfig {
 
     private static final Logger logger = Logger.getLogger(FirebaseConfig.class.getName());
 
+        @Value("${FIREBASE_SERVICE_ACCOUNT_KEY_PATH:src/main/resources/serviceAccountKey.json}")
+        private String serviceAccountPath = "src/main/resources/serviceAccountKey.json";
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        String serviceAccountPath = "src/main/resources/serviceAccountKey.json";
         logger.info("Initializing Firebase with service account key: " + serviceAccountPath);
 
         FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
