7f606e762e5022488bd393d806fdb1433fdfa4b5
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2025-04-12 09:28:00 +0700

    fix cors

--- src/main/java/com/example/smart_room/config/CorsConfig.java
+++ src/main/java/com/example/smart_room/config/CorsConfig.java
@@ -10,13 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(
-                        "http://localhost:*",
-                        "http://127.0.0.1:*",
-                        "https://api-sroom.mchieens.io.vn"
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
--- src/main/java/com/example/smart_room/config/SwaggerConfig.java
+++ src/main/java/com/example/smart_room/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,11 +16,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-
+                .servers(List.of(
+                        new Server().url("https://api-sroom.mchieens.io.vn")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", createBearerScheme()));
     }
-
     private SecurityScheme createBearerScheme() {
         return new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
